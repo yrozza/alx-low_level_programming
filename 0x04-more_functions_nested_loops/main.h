32e3917bd3bf484eaa09e0c5e253db1a9e301e08
yrozza <youssefroza3090@gmail.com>
2023-02-23 15:23:31 -0800

    u

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -12,5 +11,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-
+void print_number(int n);
 #endif
+
